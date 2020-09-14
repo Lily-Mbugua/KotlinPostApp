d8f0f65a3d7449e66455afd72de985030cee4b36
Lily Mbugua <wanjerimbugua07@gmail.com>
2020-09-14 09:33:35 +0300

    second commit

--- app/src/main/java/ke/co/postapp/api/apiClient.kt
+++ app/src/main/java/ke/co/postapp/api/apiClient.kt
@@ -1,2 +1,20 @@
 package ke.co.postapp.api
 
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+object apiClient {
+
+    var client = OkHttpClient.Builder().build()
+
+    var retrofit = Retrofit.Builder()
+        .baseUrl("https://jsonplaceholder.typicode.com/")
+        .addConverterFactory(GsonConverterFactory.create())
+        .client(client)
+        .build()
+
+    fun <T> buildService(service: Class<T>): T {
+        return retrofit.create(service)
+    }
+}
---
+++ app/src/main/java/ke/co/postapp/models/Post.kt
@@ -0,0 +1,8 @@
+package ke.co.postapp.models
+
+class Post {
+    var userId:Int,
+    var id:Int,
+    var title:String,
+    var body:String
+}
